4e1755f26ecd324c6db198b8a5c2860b102e77eb
Jerry Yang <j.yang5@elsevier.com>
2024-12-05 01:06:13 +0000

    refactor

--- scala-impl/src/main/scala/day4/CeresSearch.scala
+++ scala-impl/src/main/scala/day4/CeresSearch.scala
@@ -1,67 +1,9 @@
 package day4
 
+import day4.Direction.{allDirections, diagonalDirections}
+
 import scala.io.Source
-
-trait Elem {
-  def value: Char
-}
-object X extends Elem {
-  override def value: Char = 'X'
-}
-object M extends Elem {
-  override def value: Char = 'M'
-}
-object A extends Elem {
-  override def value: Char = 'A'
-}
-object S extends Elem {
-  override def value: Char = 'S'
-}
-object IrrelevantElem extends Elem {
-  override def value: Char = '.'
-}
-object Elem {
-  def apply(char: Char): Elem = {
-    char match {
-      case 'X' => X
-      case 'M' => M
-      case 'A' => A
-      case 'S' => S
-      case '.' => IrrelevantElem
-      case _ => throw new IllegalArgumentException(s"Invalid character: $char")
-    }
-  }
-}
-case class Pattern(pattern: Array[Elem]) {
-  def length: Int = pattern.length
-}
-object Pattern {
-  def apply(patternString: String): Pattern = {
-    new Pattern(patternString.map(char => Elem(char)).toArray)
-  }
-}
-
-case class Point(x: Int, y: Int)
-
-case class Matrix[Elem](data: Array[Array[Elem]]) {
-  def get(point: Point): Elem = {
-    data(point.x)(point.y)
-  }
-
-  def allPoints: Seq[Point] = {
-    for {
-      y <- data.indices
-      x <- data(y).indices
-    } yield Point(x, y)
-  }
-}
-object Matrix {
-  def apply[T](maze: String): Matrix[Elem] = {
-    val data = maze.split("\n").map(row => row.map(char => Elem(char)).toArray)
-    data.ensuring(mat => mat.forall(row => row.length == data.head.length), "All rows must have the same length")
-    new Matrix(data)
-  }
-}
+import scala.language.postfixOps
 
 object Reader {
   def read(filepath: String): Matrix[Elem] = {
@@ -89,19 +31,17 @@ object CeresSearch {
     val pattern = Pattern(word)
     val pointDirections = for {
       point <- matrix.allPoints
-      direction <- directions
+      direction <- allDirections
     } yield (point, direction)
 
     val height = matrix.data.length
-    val weight = matrix.data.head.length
+    val width = matrix.data.head.length
     val inboundPointDirections = pointDirections.filter { case (point, direction) =>
-        val endX = point.x + (pattern.length - 1) * direction.dx
-        val endY = point.y + (pattern.length - 1) * direction.dy
-        pointWithinBounds(Point(endX, endY), height, weight)
+      isInboundSequence(point, pattern.length, direction, height, width)
     }
 
     val matchedPointDirections = inboundPointDirections.filter { case (point, direction) =>
-      directionsWithExactMatch(pattern, matrix, point, direction)
+      pattern.`match`(matrix, point, direction)
     }
 
     matchedPointDirections.length
@@ -121,106 +61,54 @@ object CeresSearch {
     }
 
     val height = matrix.data.length
-    val weight = matrix.data.head.length
-    val pointsInBound = matrix.allPoints.filter { point =>
-      diagnalDirections.map(direction => {
-        val endPoint = Point(point.x + direction.dx, point.y + direction.dy)
-        pointWithinBounds(endPoint, height, weight)
-      }).forall(identity)
+    val width = matrix.data.head.length
+    val padding: Int = word.length / 2
+    val pointsInBound = matrix.allPoints.filter { startingPoint =>
+      isInboundCentralPoint(startingPoint, padding, height, width)
     }
-    val pattern: Array[Elem] = word.map(char => Elem(char)).toArray
+    val pattern = Pattern(word)
     val matchedPoints = pointsInBound.filter { point =>
-      findMatchForCenterPointDiagnallyInMatrix(pattern, matrix, point)
+      pattern.matchX(matrix, point)
     }
     matchedPoints.length
   }
 
-  private val diagnalDirections = Seq(DiagonalDownRight, DiagonalDownLeft, DiagonalUpRight, DiagonalUpLeft)
-
-  private def findMatchForCenterPointDiagnallyInMatrix(pattern: Array[Elem], matrix: Matrix[Elem], centerPoint: Point): Boolean = {
-    val height = matrix.data.length
-    val weight = matrix.data.head.length
-    val (x, y) = (centerPoint.x, centerPoint.y)
-    diagnalDirections.map { direction =>
-      val (dx, dy) = (direction.dx, direction.dy)
-      pattern.zip(Seq(1, 0, -1)).forall { case (elem, i) =>
-        val (newX, newY) = (x + i * dx, y + i * dy)
-        matrix.get(Point(newX, newY)) == elem
-      }
-    }.count(identity) == 2
-  }
-
   private def validateBoundary(word: String, matrix: Matrix[Elem]): Unit = {
-    // if the word is empty, return 0
     if (word.isEmpty) {
       throw new IllegalArgumentException("Word must not be empty")
     }
-    // if the matrix is empty, return 0
+
     if (matrix.data.isEmpty) {
       throw new IllegalArgumentException("Matrix must not be empty")
     }
-    // if the word outsized the matrix, return 0
+
     val maxSizeOfMatrix = {
       val height = matrix.data.length
-      val weight = matrix.data.head.length
-      Math.max(height, weight)
+      val width = matrix.data.head.length
+      Math.max(height, width)
     }
     if (word.length > maxSizeOfMatrix) {
       throw new IllegalArgumentException("Word must not be larger than the matrix")
     }
   }
 
+  private def isInboundSequence(startingPoint: Point, strLength: Int, direction: Direction, height: Int, width: Int): Boolean = {
+    val endX = startingPoint.x + (strLength - 1) * direction.dx
+    val endY = startingPoint.y + (strLength - 1) * direction.dy
+    isInboundPoint(Point(endX, endY), height, width)
+  }
 
-  trait Direction {
-    def dx: Int
-    def dy: Int
+  private def isInboundCentralPoint(startingPoint: Point, padding: Int, height: Int, width: Int): Boolean = {
+    val cornerPoints = diagonalDirections.map(direction => {
+      Point(startingPoint.x + padding * direction.dx, startingPoint.y + padding * direction.dy)
+    })
+    cornerPoints.map { endPoint =>
+      isInboundPoint(endPoint, height, width)
+    }.forall(identity)
   }
-  case object Right extends Direction {
-    override def dx: Int = 1
-    override def dy: Int = 0
-  }
-  case object Down extends Direction {
-    override def dx: Int = 0
-    override def dy: Int = 1
-  }
-  case object DiagonalDownRight extends Direction {
-    override def dx: Int = 1
-    override def dy: Int = 1
-  }
-  case object DiagonalDownLeft extends Direction {
-    override def dx: Int = -1
-    override def dy: Int = 1
-  }
-  case object DiagonalUpRight extends Direction {
-    override def dx: Int = 1
-    override def dy: Int = -1
-  }
-  case object DiagonalUpLeft extends Direction {
-    override def dx: Int = -1
-    override def dy: Int = -1
-  }
-  case object Left extends Direction {
-    override def dx: Int = -1
-    override def dy: Int = 0
-  }
-  case object Up extends Direction {
-    override def dx: Int = 0
-    override def dy: Int = -1
-  }
-  private val directions = Seq(Right, Down, DiagonalDownRight, DiagonalDownLeft, DiagonalUpRight, DiagonalUpLeft, Left, Up)
 
-  private val pointWithinBounds: (Point, Int, Int) => Boolean = (point, height, width) => {
+  private def isInboundPoint(point: Point, height: Int, width: Int): Boolean = {
     point.x >= 0 && point.x < width && point.y >= 0 && point.y < height
-  }
-  private val directionsWithExactMatch: (Pattern, Matrix[Elem], Point, Direction) => Boolean = (pattern, matrix, startingPoint, direction) => {
-    val height = matrix.data.length
-    val weight = matrix.data.head.length
-    val (x, y) = (startingPoint.x, startingPoint.y)
-    val (dx, dy) = (direction.dx, direction.dy)
-    pattern.pattern.zipWithIndex.forall { case (elem, i) =>
-      val (newX, newY) = (x + i * dx, y + i * dy)
-      matrix.get(Point(newX, newY)) == elem
-    }
   }
 }
 
---
+++ scala-impl/src/main/scala/day4/package.scala
@@ -0,0 +1,146 @@
+import day4.Direction.diagonalDirections
+import day4.{Matrix, Point}
+
+package object day4 {
+  trait Elem {
+    def value: Char
+  }
+
+  object X extends Elem {
+    override def value: Char = 'X'
+  }
+
+  object M extends Elem {
+    override def value: Char = 'M'
+  }
+
+  object A extends Elem {
+    override def value: Char = 'A'
+  }
+
+  object S extends Elem {
+    override def value: Char = 'S'
+  }
+
+  object IrrelevantElem extends Elem {
+    override def value: Char = '.'
+  }
+
+  object Elem {
+    def apply(char: Char): Elem = {
+      char match {
+        case 'X' => X
+        case 'M' => M
+        case 'A' => A
+        case 'S' => S
+        case '.' => IrrelevantElem
+        case _ => throw new IllegalArgumentException(s"Invalid character: $char")
+      }
+    }
+  }
+
+  case class Pattern(pattern: Array[Elem]) {
+    def length: Int = pattern.length
+
+    def `match`(matrix: Matrix[Elem], startingPoint: Point, direction: Direction): Boolean = {
+      val height = matrix.data.length
+      val width = matrix.data.head.length
+      val (x, y) = (startingPoint.x, startingPoint.y)
+      val (dx, dy) = (direction.dx, direction.dy)
+      pattern.zipWithIndex.forall { case (elem, i) =>
+        val (newX, newY) = (x + i * dx, y + i * dy)
+        matrix.get(Point(newX, newY)) == elem
+      }
+    }
+
+    def matchX(matrix: Matrix[Elem], centerPoint: Point): Boolean = {
+      val height = matrix.data.length
+      val width = matrix.data.head.length
+      val (x, y) = (centerPoint.x, centerPoint.y)
+      diagonalDirections.map { direction =>
+        val (dx, dy) = (direction.dx, direction.dy)
+        pattern.zip(Seq(1, 0, -1)).forall { case (elem, i) =>
+          val (newX, newY) = (x + i * dx, y + i * dy)
+          matrix.get(Point(newX, newY)) == elem
+        }
+      }.count(identity) == 2
+    }
+  }
+
+  object Pattern {
+    def apply(patternString: String): Pattern = {
+      new Pattern(patternString.map(char => Elem(char)).toArray)
+    }
+  }
+
+  case class Point(x: Int, y: Int)
+
+  case class Matrix[Elem](data: Array[Array[Elem]]) {
+    def get(point: Point): Elem = {
+      data(point.x)(point.y)
+    }
+
+    def allPoints: Seq[Point] = {
+      for {
+        y <- data.indices
+        x <- data(y).indices
+      } yield Point(x, y)
+    }
+  }
+
+  object Matrix {
+    def apply[T](maze: String): Matrix[Elem] = {
+      val data = maze.split("\n").map(row => row.map(char => Elem(char)).toArray)
+      data.ensuring(mat => mat.forall(row => row.length == data.head.length), "All rows must have the same length")
+      new Matrix(data)
+    }
+  }
+  trait Direction {
+    def dx: Int
+    def dy: Int
+  }
+  object Direction {
+    val allDirections: Seq[Direction] = Seq(Right, Down, DiagonalDownRight, DiagonalDownLeft, DiagonalUpRight, DiagonalUpLeft, Left, Up)
+    val diagonalDirections: Seq[Direction] = Seq(DiagonalDownRight, DiagonalDownLeft, DiagonalUpRight, DiagonalUpLeft)
+  }
+
+  case object Right extends Direction {
+    override def dx: Int = 1
+    override def dy: Int = 0
+  }
+
+  case object Down extends Direction {
+    override def dx: Int = 0
+    override def dy: Int = 1
+  }
+
+  case object DiagonalDownRight extends Direction {
+    override def dx: Int = 1
+    override def dy: Int = 1
+  }
+
+  case object DiagonalDownLeft extends Direction {
+    override def dx: Int = -1
+    override def dy: Int = 1
+  }
+
+  case object DiagonalUpRight extends Direction {
+    override def dx: Int = 1
+    override def dy: Int = -1
+  }
+
+  case object DiagonalUpLeft extends Direction {
+    override def dx: Int = -1
+    override def dy: Int = -1
+  }
+
+  case object Left extends Direction {
+    override def dx: Int = -1
+    override def dy: Int = 0
+  }
+
+  case object Up extends Direction {
+    override def dx: Int = 0
+    override def dy: Int = -1
+  }
+}
