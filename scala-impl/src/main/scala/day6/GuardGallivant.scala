5efd269b334b5780c6189d89fa284c177855649b
Jerry Yang <j.yang5@elsevier.com>
2024-12-07 01:20:05 +0000

    refactor for informative visited snapshot

--- scala-impl/src/main/scala/day6/GuardGallivant.scala
+++ scala-impl/src/main/scala/day6/GuardGallivant.scala
@@ -1,27 +1,46 @@
 package day6
 
+import scala.collection.mutable.ArrayBuffer
+
+case class VisitedSnapshot(var direction: Option[Direction]) {
+  def isVisited: Boolean = direction.isDefined
+  def markVisited(direction: Direction): Unit = this.direction = Some(direction)
+  def isLoopingRoute(direction: Direction): Boolean = isVisited && direction == this.direction.get
+}
 trait Tile
 case object Obstruction extends Tile
-case class Space(private var visited: Boolean = false) extends Tile {
-  def markVisited(): Unit = visited = true
+case class Space(private var visitedSnapshot: VisitedSnapshot = VisitedSnapshot(None)) extends Tile {
+  def isVisited: Boolean = visitedSnapshot.isVisited
+  def markVisited(direction: Direction): Unit = visitedSnapshot.markVisited(direction)
+  def isLoopingRoute(direction: Direction): Boolean = visitedSnapshot.isLoopingRoute(direction)
 }
+
+trait MovementResult
+case class OutOfBounds(position: Point, direction: Direction) extends MovementResult
+case class LoopingRoute(position: Point, direction: Direction) extends MovementResult
 
 trait Character extends Tile {
   def position: Point
-  def move(map: LabMap): Unit
+  def move(map: LabMap): MovementResult
 }
 class Guard(private val initialPosition: Point, private val initialDirection: Direction) extends Character {
   private var currentPosition: Point = initialPosition
   private var currentDirection: Direction = initialDirection
   override def position: Point = position
-  override def move(map: LabMap): Unit = {
+  override def move(map: LabMap): MovementResult = {
     while (!BoundaryChecker.isOutOfBounds(intendedNextPosition, map.tiles.dimension)) {
       moveOneStep(map)
       val currentTile = map.get(currentPosition)
       currentTile match {
-        case Space(_) => currentTile.asInstanceOf[Space].markVisited()
+        case Space(_) =>
+          val currentSpace = currentTile.asInstanceOf[Space]
+          if (currentSpace.isLoopingRoute(currentDirection)) {
+            return LoopingRoute(currentPosition, currentDirection)
+          }
+          currentSpace.markVisited(currentDirection)
       }
     }
+    OutOfBounds(currentPosition, currentDirection)
   }
   private def moveOneStep(map: LabMap): Unit = {
     val nextTile = map.get(intendedNextPosition)
@@ -51,8 +70,13 @@ case class LabMap(tiles: Matrix[Tile]) {
     val lines = tiles.data.map { row =>
       row.map {
         case Obstruction => "#"
-        case Space(true) => "v"
-        case Space(false) => "."
+        case Space(visitedSnapshot) => visitedSnapshot.direction match {
+          case Some(Down) => "v"
+          case Some(Right) => ">"
+          case Some(Left) => "<"
+          case Some(Up) => "^"
+          case None => "."
+        }
       }.mkString("")
     }
     LabMapRepresentation(lines)
@@ -61,7 +85,7 @@ case class LabMap(tiles: Matrix[Tile]) {
   def numberOfVisitedSpaces: Int = {
     tiles.data.map { row =>
       row.count {
-        case Space(true) => true
+        case Space(visitedSnapshot) if visitedSnapshot.isVisited => true
         case _ => false
       }
     }.sum
@@ -74,14 +98,14 @@ object LabMap {
         char match {
           case '#' => Obstruction
           case '.' => Space()
-          case 'v' |
-               '>' |
-               '<' |
-               '^' => Space(true)
+          case 'v' => Space(VisitedSnapshot(Some(Down)))
+          case '>' => Space(VisitedSnapshot(Some(Right)))
+          case '<' => Space(VisitedSnapshot(Some(Left)))
+          case '^' => Space(VisitedSnapshot(Some(Up)))
           case _ => throw new Exception(s"Invalid tile or character $char, expecting '#', '.' or 'v'")
         }
       }.toArray
-    }.toArray
+    }
     LabMap(Matrix[Tile](tiles))
   }
 }
@@ -129,11 +153,27 @@ object GuardGallivant {
     println(s"Number of distinct positions the guard visited on the map: ${labMap.toReprV1}")
   }
 
-  def findAllLoopingRouteAfterAddingOneObstacle(filepath: String): Unit = ???
+//  def findAllLoopingRouteAfterAddingOneObstacle(filepath: String): Unit = {
+//    val labMapRepr = Reader.read(filepath)
+//    val labMap = LabMap(labMapRepr)
+//    val guard = labMapRepr.initGuard()
+//    val loopingRoutes = ArrayBuffer[LoopingRoute]()
+//    while (loopingRoutes.isEmpty) {
+//      val result = guard.move(labMap)
+//      result match {
+//        case LoopingRoute(position, direction) =>
+//          loopingRoutes += LoopingRoute(position, direction)
+//        case OutOfBounds(_, _) =>
+//          throw new Exception("Guard went out of bounds")
+//      }
+//    }
+//    println(s"First looping route found: ${loopingRoutes.head}")
+//  }
 }
 
 object Main {
   def main(args: Array[String]): Unit = {
     GuardGallivant.patrolRouteCoverage("day6/puzzle-input.txt")
+//    GuardGallivant.findAllLoopingRouteAfterAddingOneObstacle("day6/map-1.txt")
   }
 }
